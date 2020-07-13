5807ac0d32e92820fe9d1f78f01a7c52ba23c18e
gwanwoodev <gwanwoodev@kakao.com>
2020-07-13 15:55:06 +0900

    queue coffee dialog issue fix

--- src/js/global.js
+++ src/js/global.js
@@ -1,3 +1,8 @@
 document.querySelector(".homeButton").addEventListener("click", () => {
     location.href = "./index.html";
 });
+
+document.querySelector("#coffee").addEventListener("click", () => {
+    dialog.open();
+});
+
